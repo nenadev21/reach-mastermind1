427ece963c85a745a8c06f3a32ee4e3d9370fa75
Dani <dvera1@babson.edu>
2023-01-18 01:14:28 -0500

    Fixed function for array comparison. Accounts for repeated values

--- src/utils/compareGuessVsRandom.js
+++ src/utils/compareGuessVsRandom.js
@@ -1,33 +1,25 @@
-export function compareGuessVsRandom(values, random) {
-  // exact values
-  const exactMatches = [];
-  const missingValues = []; // accumalator for unmatched values
-  const missingRandom = []; // accumalator for unmatched random values
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] === random[i]) {
-      exactMatches.push(values[i]);
+//helper function to count how many values two arrays have in common
+const countCommonValuesOfArrays = (arr1, arr2) =>
+  arr1.reduce((a, c) => a + arr2.includes(c), 0);
+
+export function compareGuessVsRandom(guesses, random) {
+  //check for duplicates
+  let perfectMatch = [];
+  let equalValues = [];
+  let unmatchedGuesses = [];
+  let unmatchedRandom = [];
+  for (let i = 0; i < guesses.length; i++) {
+    if (guesses[i] === random[i]) {
+      perfectMatch++;
     } else {
-      missingValues.push(values[i]);
-      missingRandom.push(random[i]);
+      unmatchedGuesses.push(guesses[i]);
+      unmatchedRandom.push(random[i]);
     }
   }
-
-  // matches values
-  const matchesByValue = [];
-  for (let i = 0; i < missingValues.length; i++) {
-    const randomIndex = missingRandom.findIndex((v) => v === missingValues[i]);
-    // if random values contains the current value, remove the value from random values and save the value in matchesByValue
-    if (randomIndex >= 0) {
-      matchesByValue.push(missingValues[i]);
-      missingRandom.splice(randomIndex, 1);
-    }
-  }
-
-  console.log(exactMatches, matchesByValue);
-  // if all checks pass, the arrays are a match
+  equalValues = countCommonValuesOfArrays(unmatchedGuesses, unmatchedRandom);
   return {
-    match: exactMatches.length === random.length,
-    exactMatches: exactMatches.length,
-    matchesByValue: matchesByValue.length,
+    gameMatch: perfectMatch === random.length,
+    perfectMatch: perfectMatch,
+    equalValues: equalValues,
   };
 }
